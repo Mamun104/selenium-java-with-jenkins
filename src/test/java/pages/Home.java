bb17f982868888eb65fc653ffd35c40ce9c82bb9
Mamunur Rashid <mamun@arogga.net>
2024-01-14 11:58:07 +0600

    uplaod

---
+++ src/test/java/pages/Home.java
@@ -0,0 +1,26 @@
+package pages;
+
+import org.junit.jupiter.api.Assertions;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+
+public class Home {
+
+    public WebDriver driver;
+    public Home(WebDriver driver) {
+
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
+
+    public void getTitle(){
+
+        String titleActual = driver.getTitle();
+        String titleExpected = "DEMOQA";
+        String titleNotExpected = "DEMOQ";
+        Assertions.assertEquals(titleExpected,titleActual);
+        Assertions.assertNotEquals(titleNotExpected,titleActual);
+        System.out.println(titleActual);
+
+    }
+}
